d2daabbabfffc28a783663e3d1791cc366e69a44
Nelson <tugoresn@gmail.com>
2024-04-02 03:20:55 -0300

    Add ClerkProvider to RootLayout && Sidebar menu

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+const AuthLayout = ({children}:{children:React.ReactNode}) => {
+  return ( <div className="h-full grid place-content-center">{children}</div> );
+}
+
+export default AuthLayout;
---
+++ src/app/(dashboard)/(routes)/search/page.tsx
@@ -0,0 +1,8 @@
+
+export default function SearchPage() {
+  return (
+    <div>
+      <h1>This is a search page</h1>
+    </div>
+  );
+}
